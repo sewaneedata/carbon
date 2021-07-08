ee839d97a669b05969ba656fe8479720997dcccf
KateKateBaker <kaitlinbaker310@gmail.com>
2021-07-08 11:51:53 -0500

    text

--- testdata.R
+++ testdata.R
@@ -1,22 +1,20 @@
 library (dplyr)
 library(googlesheets4)
 library (tidyr)
+
+# This converts each speadsheet into a dataframe and compules a list. 
+
 data_list <- list()
-sheet_numbers <- 5:53
+sheet_numbers <- 5:54
 for(i in 1:length(sheet_numbers)){
   sheet_id <- sheet_numbers[i]
   temp <- read_sheet('https://docs.google.com/spreadsheets/d/1QMTqhd-llPQH_2AfaiV6klHXpe1alPE7Eo85UU82sP0/edit?usp=sharing', sheet = sheet_id)
-  temp <- temp[,1:30]
+  temp <- temp[,1:6]
   data_list[[i]] <- temp
 }
 dat <- do.call('rbind', data_list)
 
-# Info on all of the data- household names and numbers 
-meta <- gs4_get("https://docs.google.com/spreadsheets/d/1QMTqhd-llPQH_2AfaiV6klHXpe1alPE7Eo85UU82sP0/edit?usp=sharing")
 
-dat <- dat %>% select (- "...16")
-dat <- dat %>% select (-"...25")
 dat <- dat %>% drop_na()
 # Data now ready for app, no NAs
-
 
